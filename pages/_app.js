6906c7e0fbd79ad281204e3427e076cd4f1de883
Pablo Ramos <pablo-362@hotmail.com>
2022-04-13 22:16:56 -0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Head from "next/head"
 import Navbar from "../src/components/Navbar/Navbar"
 import Footer from "../src/components/Footer/Footer"
+import Script from "next/script"
 import "../styles/style.css"
 
 function MyApp({ Component, pageProps }) {
@@ -12,6 +13,19 @@ function MyApp({ Component, pageProps }) {
 				<meta name="Pablo Ramos frontend developer blog" content="Blog Pablo Ramos" />
 				{/* <link rel="icon" href="/favicon-32x32.png" /> */}
 			</Head>
+			<Script 
+				async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} 
+				strategy="afterInteractive"
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+			>
+				{`window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', ${process.env.GOOGLE_ANALYTICS})`}
+			</Script>
 			<ChakraProvider>
 				<Navbar />
 				<Component {...pageProps} />
